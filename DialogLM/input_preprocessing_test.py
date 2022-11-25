d16dfad250ccbf38d2369caf61e07baa536977ea
Gunhee-Cho <38035043+Gunhee-Cho@users.noreply.github.com>
2022-11-25 17:49:22 +0900

    Add files via upload

---
+++ DialogLM/input_preprocessing_test.py
@@ -0,0 +1,25 @@
+import pandas as pd
+#import re
+
+data=pd.read_csv('before_preprocessed_data/test_seg_movie_dataset_final.csv')
+
+content_data=data[['Movie_name','content']]
+content_data
+
+# csv to json
+df= content_data
+df.rename(columns={"content":"src", "Movie_name":"tgt"}, inplace=True)
+df=df[["src", "tgt"]]
+df = df.dropna().reset_index(drop=True)
+
+df["src"] = df["src"].str.replace(pat=r'([A-Z]{2,})[ \n][(]([a-z]{2,})[)]\n([A-Z])', repl=r'\1 : \3', regex=True)
+df["src"] = df["src"].str.replace(pat=r'([A-Z]{2,})\n([A-Z])', repl=r'\1 : \2', regex=True)
+df["src"] = df["src"].str.replace(pat='\n\n', repl=' ', regex=False)
+df["src"] = df["src"].str.replace(pat='\n', repl=' ', regex=False)
+df["src"] = df["src"].str.replace(pat=r'''[^A-Za-z0-9\n:'".?!\(\) ]''', repl=r'', regex=True)
+df["src"] = df["src"].str.replace(pat='\\n', repl='', regex=False)
+df["src"] = df["src"].str.replace(pat="\'", repl="'", regex=False)
+df["src"] = df["src"].str.replace(pat='\"', repl='"', regex=False)
+df["src"] = df["src"].str.replace(pat=r'([ \t\r\n\v\f]{1,})', repl=r' ', regex=True)
+
+df.to_json ("data/scene_test_final_practice.json")
